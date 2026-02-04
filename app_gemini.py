3bd26e2395462d463e368154f6862eaeeda31e34
Daniel Felipe Chávez González <danielf.chavezg@konradlorenz.edu.co>
2026-02-03 21:07:12 -0500

    Initial implementation
    initial implementation of Gemini API connection

---
+++ app_gemini.py
@@ -0,0 +1,28 @@
+import os
+from urllib import response
+from google import genai
+from dotenv import load_dotenv
+# 1. Cargar configuración de variables de entorno
+load_dotenv()
+clave_api = os.getenv("GEMINI_API_KEY")
+# 2. Inicializar el Cliente
+# Este cliente gestiona la conexión
+client = genai.Client(api_key=clave_api)
+def ejecutar_consulta():
+    print("🚀 Conectando con el motor de Gemini ...")
+
+    try:
+        # 3. Llamada directa al servicio de modelos
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents="Cuenta un chiste corto sobre programadores.",
+        )
+        print("\n--- Respuesta Recibida ---")
+        print(response.text)
+        print("--------------------------")
+        
+    except Exception as e:
+        print(f"❌ Ocurrió un error en la conexión: {e}")
+    
+if __name__ == "__main__":
+    ejecutar_consulta()
